22f7ecc17c9a0f8c79ee214d32a938d4b5f5f81f
Asad Khan <mrasad10khan@gmail.com>
2024-06-14 10:52:59 +0500

    Adding footer colors

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -4,15 +4,25 @@ import "./Footer.css";
 function Footer() {
   return (
     <div className="footer-sec">
-      <div className="footer-size">
-        <button className="btn">8</button>
-        <button className="btn">8.5</button>
-        <button className="btn">9</button>
-        <button className="btn">9.5</button>
-        <button className="btn">10</button>
-        <button className="btn">10.5</button>
-        <button className="btn">11</button>
-        <button className="btn">12</button>
+      <div className="footer-left">
+        <div className="footer-size">
+          <button className="btn">8</button>
+          <button className="btn">8.5</button>
+          <button className="btn">9</button>
+          <button className="btn">9.5</button>
+          <button className="btn">10</button>
+          <button className="btn">10.5</button>
+          <button className="btn">11</button>
+          <button className="btn">12</button>
+        </div>
+
+        <div className="footer-colors">
+          <button className="btn-colors"></button>
+          <button className="btn-colors"></button>
+          <button className="btn-colors"></button>
+          <button className="btn-colors"></button>
+          <button className="btn-colors"></button>
+        </div>
       </div>
     </div>
   );
